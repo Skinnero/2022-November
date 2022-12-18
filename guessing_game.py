0c23fc4dbb34a426793a2308f504343acb21c6f6
Skinnero <plskinero@gmail.com>
2022-12-18 13:52:33 +0100

    guessing_game refactoring

---
+++ guessing_game.py
@@ -0,0 +1,29 @@
+import math
+import random
+
+GAME_TURNS = 10
+MIN = 1
+FIRST_MAX = 99
+SECOND_MAX = 49
+def random_choices(min,max,game_turns):
+    choices_list = [random.randint(min,max) for num in range(game_turns)] # Creates a list with random choices
+    return choices_list # Returns a random 
+
+def ask_for_number(choice,min,max):
+    for i in range(len(choice)):
+        guess = None
+        while choice[i] != guess:
+            guess = int(input(f"Enter an integer from {min} to {max}: "))
+            if guess < choice[i]:
+                print("guess is low") 
+            else:
+                print("guess is high")
+        print("you guessed it!")
+def main():       
+    choices_1 = random_choices(MIN,FIRST_MAX,GAME_TURNS) 
+    ask_for_number(choices_1,MIN,FIRST_MAX)       
+    choices_2 = random_choices(MIN,SECOND_MAX,GAME_TURNS)
+    ask_for_number(choices_2,MIN,SECOND_MAX)
+
+if __name__ == '__main__':
+    main()
